a3be74d52c51e68a8d4b92a06b1e193d46e3c11c
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-01 00:44:23 +0900

    Update engine.rs

--- src/engine.rs
+++ src/engine.rs
@@ -77,8 +77,8 @@ impl Engine {
                 } else if verb.last()?.0 == "ke-s" {
                     match verb.first()?.0.as_str() {
                         "yes" => {
-                            if self.eval(&*subj)?.as_bool()? {
-                                self.eval(&*obj)
+                            if self.eval(&*obj)?.as_bool()? {
+                                self.eval(&*subj)
                             } else {
                                 Some(Value::Null)
                             }
