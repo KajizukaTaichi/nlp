c63d46ed0fff2bbed18eeee96e15f89906d9589b
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-01 00:09:12 +0900

    Update engine.rs

--- src/engine.rs
+++ src/engine.rs
@@ -41,6 +41,7 @@ impl Engine {
                 let lhs = self.eval(&*subj)?;
                 let rhs = self.eval(&*obj)?;
 
+                // 数値
                 if verb.first()?.0 == "nam" {
                     Some(match verb.last()?.0.as_str() {
                         "a*d" => Value::Number(lhs.as_number()? + rhs.as_number()?),
@@ -49,11 +50,19 @@ impl Engine {
                         "div" => Value::Number(lhs.as_number()? / rhs.as_number()?),
                         _ => return None,
                     })
+                // 文字列
                 } else if verb.first()?.0 == "car" && verb.get(1)?.0 == "a-l" {
                     Some(match verb.last()?.0.as_str() {
                         "a*d" => Value::String(lhs.as_string()? + &rhs.as_string()?),
                         _ => return None,
                     })
+                } else if verb.first()?.0 == "est" {
+                    if self.is_ask {
+                        Some(Value::Bool(format!("{lhs:?}") == format!("{rhs:?}")))
+                    } else {
+                        self.scope.insert(lhs.as_string()?, rhs.clone());
+                        Some(rhs)
+                    }
                 } else if verb.first()?.0 == "est" {
                     if self.is_ask {
                         Some(Value::Bool(format!("{lhs:?}") == format!("{rhs:?}")))
@@ -85,11 +94,18 @@ impl Engine {
                 word,
                 own: None,
                 adj: _,
-            } => word
-                .format()
-                .parse()
-                .map(|x| Some(Value::Number(x)))
-                .unwrap_or(Some(Value::String(word.format()))),
+            } => {
+                let word = word.format();
+                if let Ok(n) = word.parse::<f64>() {
+                    Some(Value::Number(n))
+                } else if word == "yes" {
+                    Some(Value::Bool(true))
+                } else if word == "ne" {
+                    Some(Value::Bool(false))
+                } else {
+                    Some(Value::String(word))
+                }
+            }
             _ => None,
         }
     }
